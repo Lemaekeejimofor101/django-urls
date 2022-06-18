9d847295a298340c173f6d60363018361eb69a5f
Ife Jeremiah <jerryroboy16@gmail.com>
2022-06-18 22:21:17 +0100

    Built django app, create super admin, changed admin url

--- lemaeke_faith_ejimofor/lemaeke_faith_ejimofor/settings.py
+++ lemaeke_faith_ejimofor/lemaeke_faith_ejimofor/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-se!2!2$p@6yph9d9)(!lf0_mv!vao1!@4v!o)8qwp24$fy%r0!'
+SECRET_KEY = 'django-insecure-bqvx_!)!#4&d84a6clq7&2zodkq6@wxn4zq$@=q(^g!!vapm(g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
